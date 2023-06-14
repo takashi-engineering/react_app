e6f1adc83a07c72393c618021946fd1b4128f585
takashi <pero857942632@gmail.com>
2023-06-14 23:18:29 +0900

    関数コンポーネントに引数を渡す

--- src/pages/hooks/HookSample2.js
+++ src/pages/hooks/HookSample2.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
+
 function HookSample2() {
+    const [msg] = useState("This is sample message!")
     return (
         <div>
             <h1 className="bg-primary text-white display-4">React</h1>
             <div className="container">
                 <h4 className="my-3">Hooks sample</h4>
-                <AlertMessage />
-                <CardMessage />
+                <AlertMessage message={msg} />
+                <CardMessage message={msg} />
             </div>
         </div>
     )
@@ -13,14 +16,18 @@ function HookSample2() {
 
 export default HookSample2;
 
-function AlertMessage() {
+function AlertMessage(props) {
     return (
-        <div className="alert alert-primary h5 text-primary">This is Alert Message!</div>
+        <div className="alert alert-primary h5 text-primary">
+            {props.message}
+        </div>
     )
 }
 
-function CardMessage() {
+function CardMessage(props) {
     return (
-        <div className="card p-3 h5 border-primary text-center">This is Card Message!</div>
+        <div className="card p-3 h5 border-primary text-center">
+            {props.message}
+        </div>
     )
 }
