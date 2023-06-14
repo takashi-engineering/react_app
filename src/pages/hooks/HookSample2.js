ab8e5e323432cf888c157cc0ec226ae3583efa25
takashi <pero857942632@gmail.com>
2023-06-14 23:24:30 +0900

    Stateを使って値を更新する

--- src/pages/hooks/HookSample2.js
+++ src/pages/hooks/HookSample2.js
@@ -1,7 +1,13 @@
 import { useState } from "react";
 
 function HookSample2() {
-    const [msg] = useState("This is sample message!")
+    const [msg, setMsg] = useState("This is sample message!")
+
+    // ボタンが押された時のアクション
+    const doAction = () =>{
+        let res = window.prompt('type your name:')
+        setMsg("Hello, " + res + "!!")
+    }
     return (
         <div>
             <h1 className="bg-primary text-white display-4">React</h1>
@@ -9,6 +15,9 @@ function HookSample2() {
                 <h4 className="my-3">Hooks sample</h4>
                 <AlertMessage message={msg} />
                 <CardMessage message={msg} />
+                <div className="text-center">
+                    <button onClick={doAction} className="btn btn-primary">Click me!</button>
+                </div>
             </div>
         </div>
     )
